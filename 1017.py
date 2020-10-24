6d06cd39055a8e582be3d1e3a2e9c5b61f6bf8cf
Alancow <71136361+Alancow@users.noreply.github.com>
2020-10-24 11:48:32 +0800

    Add files via upload

--- 1017.py
+++ 1017.py
@@ -2,17 +2,6 @@ from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
 
-mc.setBlock(x,y+1,z,57)
-mc.setBlock(x,y+2,z,57)
-mc.setBlock(x,y+3,z,57)
-mc.setBlock(x,y+4,z,57)
-mc.setBlock(x,y+5,z,57)
-mc.setBlock(x,y+6,z,57)
-mc.setBlock(x,y+7,z,57)
-mc.setBlock(x,y+8,z,57)
-mc.setBlock(x,y+9,z,57)
-mc.setBlock(x,y+10,z,57)
-mc.setBlock(x,y+11,z,57)
 
 
 x,y,z = mc.player.getTilePos()
