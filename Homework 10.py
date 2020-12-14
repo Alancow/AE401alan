15bc662d3fbe8bac96e4d1eae4375733d85c511c
Alancow <71136361+Alancow@users.noreply.github.com>
2020-12-14 17:57:57 +0800

    Add files via upload

---
+++ Homework 10.py
@@ -0,0 +1,27 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+import random
+
+
+x,y,z = mc.player.getTilePos()
+
+for i in range(20):
+    r = random.randrange(1, 5)
+    e = random.randrange(0, 16)
+    
+    
+    if r == 1:
+        mc.setBlocks(x,y,z,x+4,y,z,35,e)
+        x = x +4
+       
+    elif r == 2:
+        mc.setBlocks(x,y,z,x-4,y,z,35,e)
+        x = x -4
+    
+    elif r == 3:
+        mc.setBlocks(x,y,z,x,y+4,z,35,e)
+        y = y +4
+       
+    elif r == 4:
+        mc.setBlocks(x,y,z,x,y-4,z,35,e)
+        y = y -4
